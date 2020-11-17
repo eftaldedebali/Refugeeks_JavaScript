1052331ff4c95341b51e3cdc1d049b6f36254c67
eftaldedebali <eftal.dedebali@stud.hs-hannover.de>
2020-11-17 16:38:34 +0100

    17.11.2020_search

--- Gruppenarbeit/js/search.js
+++ Gruppenarbeit/js/search.js
@@ -4,3 +4,20 @@
  * 2. Clear the input by clicking the "x" button
  * 
  */
+
+document.getElementsByClassName("px-6 w-full relative text-gray-600")[0].setAttribute("id", "searchbar");
+document.getElementsByClassName("search-btn pl-3 inline-block no-underline hover:text-black")[0].href = "#search";
+document.getElementsByClassName("search-btn pl-3 inline-block no-underline hover:text-black")[0].setAttribute("onclick", "searchHide()");
+
+function searchHide(){
+    document.getElementById("searchbar").classList.toggle("hidden");
+}
+
+document.getElementsByClassName("w-full bg-gray-100 h-10 px-5 pr-10 rounded-full text-sm focus:outline-none")[0].setAttribute("id", "searchinput");
+
+function clearInput(){
+    document.getElementById("searchinput").value = "";
+}
+
+document.getElementsByClassName("absolute right-0 top-0 mt-1 mr-8")[0].setAttribute("onclick", "clearInput()");
+
